fa5b74f9fac82d75755c7dab4282f92139163a59
3nf4n7 <myfirsttwink1@gmail.com>
2024-06-09 18:06:31 +0300

    admin dashbord

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,5 +1,11 @@
 import dotenv from "dotenv";
 import path from "path";
+import type { InitOptions } from "payload/config";
+import payload from "payload";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename);
 
 dotenv.config({
     path: path.resolve(__dirname, "../.env")
@@ -8,7 +14,37 @@ dotenv.config({
 let cached = (global as any).payload
 
 if (!cached) {
-    
+    cached = (global as any).payload = {
+        client: null,
+        promise: null,
+    }
 }
 
-export const getPayoloadClient = async () => {}
+interface Args {
+    initOptions?: Partial<InitOptions>
+}
+
+export const getPayoloadClient = async ({initOptions}: Args = {}) => {
+    if (!process.env.PAYLOAD_SECRET) {
+        throw new Error('PAYLOAD_SECRET is missing')
+    }
+
+    if (cached.client) {
+        return cached.clinet;
+    }
+
+    if (!cached.promise) {
+        cached.promise = payload.init({
+            secret: process.env.PAYLOAD_SECRET,
+            local: initOptions?.express ? false : true,
+            ...(initOptions || {})
+        });
+    }
+
+    try {
+        cached.client = await cached.promise;
+    } catch (e: unknown) {
+        cached.promise = null;
+        throw e;
+    }
+}
